afe3b14f432b7b02c109dd4faf797f155ade263f
Aina Niemetz <aina.niemetz@gmail.com>
2021-11-12 14:19:07 -0800

    Add initial version of publications.

---
+++ _scripts/gen_refs_yml.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+pandoc vita/vita.bib -s -f biblatex -t markdown \
+  | sed 's/^nocite: "\[@\*\]"//' \
+  | sed 's/^---//' \
+  | sed 's/issued: \([0-9][0-9][0-9][0-9]\)-\([0-9][0-9]\)/issued:\n  - year: \1\n    month: \2/g' \
+  | sed 's/issued: \([0-9][0-9][0-9][0-9]\)/issued:\n  - year: \1\n    month: 01/g' \
+  | sed 's/month: 0/month: /' \
+  | sed 's/\$\([0-9]\)*^{\(.*\)}\$/\1<sup>\2<\/sup>/' \
+  | sed 's/\(title:.*\)\[/\1/g' \
+  | sed 's/\]{\.nocase}//g' \
+  > _data/publications.yaml
