91a83b6d3193d6ef749ed0d65ab1458eeb53e6a1
Aina Niemetz <aina.niemetz@gmail.com>
2021-11-12 15:16:51 -0800

    Add references without Clark.

--- _scripts/gen_refs_yml.sh
+++ _scripts/gen_refs_yml.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-pandoc vita/vita.bib -s -f biblatex -t markdown \
+pandoc _refs/vita/vita.bib -s -f biblatex -t markdown \
   | sed 's/^nocite: "\[@\*\]"//' \
   | sed 's/^---//' \
   | sed 's/issued: \([0-9][0-9][0-9][0-9]\)-\([0-9][0-9]\)/issued:\n  - year: \1\n    month: \2/g' \
@@ -9,3 +9,14 @@ pandoc vita/vita.bib -s -f biblatex -t markdown \
   | sed 's/\(title:.*\)\[/\1/g' \
   | sed 's/\]{\.nocase}//g' \
   > _data/publications.yaml
+pandoc _refs/refs.bib -s -f biblatex -t markdown \
+  | sed 's/^nocite: "\[@\*\]"//' \
+  | sed 's/^---//' \
+  | sed 's/^references://' \
+  | sed 's/issued: \([0-9][0-9][0-9][0-9]\)-\([0-9][0-9]\)/issued:\n  - year: \1\n    month: \2/g' \
+  | sed 's/issued: \([0-9][0-9][0-9][0-9]\)/issued:\n  - year: \1\n    month: 01/g' \
+  | sed 's/month: 0/month: /' \
+  | sed 's/\$\([0-9]\)*^{\(.*\)}\$/\1<sup>\2<\/sup>/' \
+  | sed 's/\(title:.*\)\[/\1/g' \
+  | sed 's/\]{\.nocase}//g' \
+  >> _data/publications.yaml
