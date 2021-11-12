a361a21e55c2dac9f136ac5d3880319902b7e544
Mathias Preiner <mathias.preiner@gmail.com>
2021-11-12 11:45:18 -0800

    Add upload script.

---
+++ _scripts/upload.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf _site
+bundle-2.7 exec jekyll build
+rsync -av _site/* barrett1:/afs/cs/group/barrettlab/centaur_www/foo
--- sync.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-rsync -av _site/* barrett2:/afs/cs.stanford.edu/group/barrettlab/centaur_www/foo/
