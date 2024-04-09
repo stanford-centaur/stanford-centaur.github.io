8706352a012cba81bcb711e9c550567a732011b9
Mathias Preiner <mathias.preiner@gmail.com>
2024-04-09 07:50:22 -0700

    Remove obsolete upload scripts.

--- _scripts/upload-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf _site
-docker run --rm --volume="$PWD:/srv/jekyll" --publish [::1]:4000:4000 jekyll/jekyll jekyll build
-rsync -av _site/* barrett1:/afs/cs/group/barrettlab/centaur_www/
--- _scripts/upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf _site
-bundle-2.7 exec jekyll build
-rsync -av _site/* barrett1:/afs/cs/group/barrettlab/centaur_www/
